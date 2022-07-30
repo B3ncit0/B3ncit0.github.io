37012ceeef57c9a6758c434372c33412be2738d8
BLUE110601 <57922440+BLUE110601@users.noreply.github.com>
2022-07-30 14:39:45 -0600

    fleshed out the commits and details pages

--- src/app/content/pages/selection-details/selection-details.component.ts
+++ src/app/content/pages/selection-details/selection-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DetailSelection } from 'src/models/selection-detail.model';
+import { SelectionOption } from 'src/models/selection.model';
+import { SelectionService } from 'src/services/selection.service';
 
 @Component({
   selector: 'app-selection-details',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selection-details.component.css']
 })
 export class SelectionDetailsComponent implements OnInit {
+  currentSelection = new SelectionOption();
+  currentDetails: DetailSelection[] = [];
+  selectedDetails: DetailSelection[] = [];
 
-  constructor() { }
+  constructor(private selectionService: SelectionService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let currentId = parseInt(this.route.snapshot.paramMap.get('id')!);
+
+    this.selectionService.getSelectionOption(currentId).subscribe(data => this.currentSelection = data);
+
+    this.selectionService.getDetails(currentId).subscribe(data => {
+      this.currentDetails = data.filter(detail => detail.parentId == currentId);
+     });
+  }
+
+  onDetailSelected(detail: DetailSelection) {
+    let index = this.selectedDetails.findIndex(d => d.id == detail.id);
+    
+    // if the detail does exist, delete it, otherwise add it
+    index != -1 ? this.selectedDetails.splice(index, 1): this.selectedDetails.push(detail);
+    console.log(this.selectedDetails);
   }
 
 }
